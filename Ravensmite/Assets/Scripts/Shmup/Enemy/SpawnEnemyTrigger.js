02e0c8fe446d1a2f0534eb5cd98a3c20f9ffb367
Abyll <abyll@neejean.org>
2014-07-06 14:54:09 -0600

    Missed the spawntrigger script last commit. Currently doesn't work, as seen with BossSpawn in testscene at end of level path.

---
+++ Ravensmite/Assets/Scripts/Shmup/Enemy/SpawnEnemyTrigger.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var enemyToSpawn: GameObject;
+private var triggered = false;
+
+function Start () {
+
+}
+
+function Update () {
+	
+}
+
+function OnTriggerEnter() {
+	if(!triggered) {
+		triggered = true;
+		Instantiate(enemyToSpawn, transform.position, transform.rotation);
+	}
+}
